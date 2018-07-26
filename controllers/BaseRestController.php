e3786edb4ae8f745424e65e47b69f09c25bee1b8
mac <mac@mac.com>
2018-07-26 15:12:40 +0800

    1

---
+++ controllers/BaseRestController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/7/26
+ * Time: 15:12
+ */
+
+namespace yhuaqiang\xlib\controllers;
+
+
+use yii\rest\ActiveController;
+
+class BaseRestController extends ActiveController
+{
+
+}
